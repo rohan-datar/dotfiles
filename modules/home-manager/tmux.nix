5e555b93ef19be588c8b55f90df43f03404237fa
Rohan Datar <me@rohandatar.com>
2024-10-27 00:06:40 -0500

    add extra tmux config

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -76,5 +76,31 @@ in
         '';
       }
     ];
+
+    extraConfig = ''
+      # kill sessions easily
+      bind X confirm-before kill-session
+
+      unbind %
+      bind | split-window -h
+
+      unbind '"'
+      bind - split-window -v
+
+      bind j resize-pane -D 5
+      bind k resize-pane -U 5
+      bind l resize-pane -R 5
+      bind h resize-pane -L 5
+
+      bind -r m resize-pane -Z
+
+
+      bind-key -T copy-mode-vi 'v' send -X begin-selection # start selecting text with "v"
+      bind-key -T copy-mode-vi 'y' send -X copy-selection # copy text with "y"
+
+      unbind -T copy-mode-vi MouseDragEnd1Pane # don't exit copy mode when dragging with mouse
+
+      set-option -g status-position top
+    '';
   };
 }
