5e9738775746bddc7d7a0eb3d19c1d4709d56df1
Rohan Datar <me@rohandatar.com>
2024-10-29 22:59:41 -0500

    43 current  2024-10-27 23:33:18  24.11.20241023.2768c7d   6.11.5                          *

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -41,7 +41,7 @@ in {
       bindkey '^n' history-search-forward
       # Completion styling
       zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-      zstyle ':completion:*' list-colors "${s.:. LS_COLORS}"
+      zstyle ':completion:*' list-colors "''${s.:. LS_COLORS}"
       zstyle ':completion:*' menu no
       zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
       zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
