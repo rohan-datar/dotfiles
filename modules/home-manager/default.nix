91b0a1da7cdfaa436cdff8e14338c1618afe14e4
Rohan Datar <me@rohandatar.com>
2024-10-29 23:23:58 -0500

    46 current  2024-10-29 23:23:54  24.11.20241023.2768c7d   6.11.5                          *

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -41,6 +41,7 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+    pkgs.oh-my-zsh
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
