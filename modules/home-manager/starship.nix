261583bb9af58c9421f2b626e8a4d03b3aa6b064
Rohan Datar <me@rohandatar.com>
2024-11-02 16:04:45 -0500

    50 current  2024-11-02 16:04:42  24.11.20241029.807e915   6.11.5                          *

---
+++ modules/home-manager/starship.nix
@@ -0,0 +1,25 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    enableTransience = true;
+    settings = {
+      add_newline = false;
+      format = lib.concatStrings [
+        "$line_break"
+        "$package"
+        "$line_break"
+        "$character"
+      ];
+      scan_timeout = 10;
+      character = {
+        success_symbol = "➜";
+        error_symbol = "➜";
+      };
+    };
+  };
+}
