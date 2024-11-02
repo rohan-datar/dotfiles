5314307a235c385a02dc1570bf9c9c5e4b9b363d
Rohan Datar <me@rohandatar.com>
2024-11-02 17:19:04 -0500

    64 current  2024-11-02 17:19:00  24.11.20241029.807e915   6.11.5                          *

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -36,7 +36,6 @@
       bindkey '^n' history-search-forward
       # Completion styling
       zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-      # zstyle ':completion:*' list-colors "''${s.:. LS_COLORS}"
       zstyle ':completion:*' menu no
       zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
       zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
@@ -49,7 +48,6 @@
       # xref: https://github.com/starship/starship/issues/3418
       type starship_zle-keymap-select >/dev/null || \
         {
-          echo "Load starship"
           eval "$(/home/rdatar/.nix-profile/bin/starship init zsh)"
         }
     '';
