f9be7a5da637478f8a39067bd13d4e665f2fee4d
Rohan Datar <me@rohandatar.com>
2024-11-02 21:46:21 -0500

    65 current  2024-11-02 17:34:48  24.11.20241029.807e915   6.11.5                          *

--- hosts/home-desktop/configuration.nix
+++ hosts/home-desktop/configuration.nix
@@ -60,7 +60,7 @@ in {
     home-manager
     wl-clipboard
     ungoogled-chromium
-    obsidian-wayland
+    obsidian
     inputs.zen-browser.packages."${system}".specific
     gnomeExtensions.dash-to-dock
     libnotify
