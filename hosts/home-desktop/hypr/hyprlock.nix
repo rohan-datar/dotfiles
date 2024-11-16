a8d9792f475349304dd46fef065549a483e40d23
Rohan Datar <me@rohandatar.com>
2024-11-16 01:44:24 -0600

    104 current  2024-11-16 01:28:51  24.11.20241111.dc460ec   6.11.7                          *

--- hosts/home-desktop/hypr/hyprlock.nix
+++ hosts/home-desktop/hypr/hyprlock.nix
@@ -8,74 +8,73 @@
     };
 
     settings = {
-# GENERAL
-general = {
-    disable_loading_bar = true;
-    hide_cursor = true;
-};
+      # GENERAL
+      general = {
+        disable_loading_bar = true;
+        hide_cursor = true;
+      };
 
-# BACKGROUND
-background = {
-    path = "~/.config/backgrounds/shaded.png";
-    blur_passes = 2;
-    color = "$base";
-};
+      # BACKGROUND
+      background = {
+        path = "~/.local/share/backgrounds/Cloudsnight.jpg";
+        blur_passes = 2;
+        color = "$base";
+      };
 
-# TIME
-label = {
-    text = ''cmd[update:30000] echo "$(date +"%R")"'';
-    color = "$text";
-    font_size = 90;
-    font_family = "$font";
-    position = "-30, 0";
-    halign = "right";
-    valign = "top";
-};
+      # TIME
+      label = {
+        text = ''cmd[update:30000] echo "$(date +"%R")"'';
+        color = "$text";
+        font_size = 90;
+        font_family = "$font";
+        position = "-30, 0";
+        halign = "right";
+        valign = "top";
+      };
 
-# DATE
-label = {
-    text = ''cmd[update:43200000] echo "$(date +"%A, %d %B %Y")"'';
-    color = "$text";
-    font_size = 25;
-    font_family = "$font";
-    position = "-30, -150";
-    halign = "right";
-    valign = "top";
-};
+      # DATE
+      label = {
+        text = ''cmd[update:43200000] echo "$(date +"%A, %d %B %Y")"'';
+        color = "$text";
+        font_size = 25;
+        font_family = "$font";
+        position = "-30, -150";
+        halign = "right";
+        valign = "top";
+      };
 
-# USER AVATAR
+      # USER AVATAR
 
-image = {
-    path = "~/.face";
-    size = 100;
-    border_color = "$accent";
-;
-    position = "0, 75";
-    halign = "center";
-    valign = "center";
-};
+      # image = {
+      #     path = "~/.face";
+      #     size = 100;
+      #     border_color = "$accent";
+      #     position = "0, 75";
+      #     halign = "center";
+      #     valign = "center";
+      # };
 
-# INPUT FIELD
-input-field = {
-    size = "300, 60";
-    outline_thickness = "4";
-    dots_size = "0.2";
-    dots_spacing = "0.2";
-    dots_center = "true";
-    outer_color = "$accent";
-    inner_color = "$surface0";
-    font_color = "$text";
-    fade_on_empty = "false";
-    placeholder_text = ''<span foreground="##$textAlpha"><i>󰌾 Logged in as </i><span foreground="##$accentAlpha">$USER</span></span>'';
-    hide_input = "false";
-    check_color = "$accent";
-    fail_color = "$red";
-    fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
-    capslock_color = "$yellow";
-    position = "0, -35";
-    halign = "center";
-    valign = "center";
-};
+      # INPUT FIELD
+      input-field = {
+        size = "300, 60";
+        outline_thickness = "4";
+        dots_size = "0.2";
+        dots_spacing = "0.2";
+        dots_center = "true";
+        outer_color = "$accent";
+        inner_color = "$surface0";
+        font_color = "$text";
+        fade_on_empty = "false";
+        placeholder_text = ''<span foreground="##$textAlpha"><i>󰌾 Logged in as </i><span foreground="##$accentAlpha">$USER</span></span>'';
+        hide_input = "false";
+        check_color = "$accent";
+        fail_color = "$red";
+        fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
+        capslock_color = "$yellow";
+        position = "0, -35";
+        halign = "center";
+        valign = "center";
+      };
     };
   };
 }
