ec1fa2e155ddcd27a5b942aac8a414796032d460
Rohan Datar <me@rohandatar.com>
2024-11-16 00:21:10 -0600

    94 current  2024-11-16 00:21:05  24.11.20241111.dc460ec   6.11.7                          *

--- hosts/home-desktop/hypr/waybar.nix
+++ hosts/home-desktop/hypr/waybar.nix
@@ -38,7 +38,7 @@
           format-linked = "{ifname} (No IP) ";
           format-disconnected = "<span size='13000' foreground='#f5e0dc'>  </span>Disconnected";
           tooltip-format-wifi = "Signal Strength: {signalStrength}%";
-          on-click = "nmtui";
+          on-click = "nm-applet";
         };
 
         "pulseaudio" = {
