81f46f1c91dd6d5ca73daea14547323c38fd5bd8
Rohan Datar <me@rohandatar.com>
2024-11-17 00:06:56 -0600

    122 current  2024-11-17 00:06:50  25.05.20241115.5e4fbfb   6.11.8                          *

--- hosts/home-desktop/system.nix
+++ hosts/home-desktop/system.nix
@@ -88,6 +88,8 @@
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
 
+  services.openssh.enable = true;
+
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
