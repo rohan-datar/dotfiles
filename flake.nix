c6e156a52fe31caeb1c01e7bdddee98b31a8981c
Rohan Datar <me@rohandatar.com>
2024-11-17 17:28:57 -0600

    fix filename

--- flake.nix
+++ flake.nix
@@ -26,66 +26,67 @@
     hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nix-darwin,
-    nix-homebrew,
-    home-manager,
-    catppuccin,
-    ...
-  } @ inputs: let
-    home-desktop = "home-desktop";
-    macbook = "macbook";
-  in {
-    # config for desktop
-    nixosConfigurations.${home-desktop} = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/${home-desktop}/configuration.nix
-        inputs.catppuccin.nixosModules.catppuccin
-      ];
-    };
-
-    # config for macbook
-    darwinConfigurations.${macbook} = nix-darwin.lib.darwinSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/${macbook}/configuration.nix
-        # inputs.home-manager.darwinModules.default
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            enable = true;
-            # Apple Silicon Only
-            enableRosetta = true;
-
-            user = "rohandatar";
-
-            autoMigrate = true;
-          };
-        }
-      ];
-    };
-
-    homeConfigurations = {
-      rdatar = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        extraSpecialArgs = {inherit inputs;};
+  outputs =
+    { self
+    , nixpkgs
+    , nix-darwin
+    , nix-homebrew
+    , home-manager
+    , catppuccin
+    , ...
+    } @ inputs:
+    let
+      home-desktop = "home-desktop";
+      macbook = "macbook";
+    in
+    {
+      # config for desktop
+      nixosConfigurations.${home-desktop} = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
         modules = [
-          ./hosts/${home-desktop}/home.nix
-          catppuccin.homeManagerModules.catppuccin
+          ./hosts/${home-desktop}/configuration.nix
+          inputs.catppuccin.nixosModules.catppuccin
         ];
       };
 
-      rohandatar = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages."aarch64-darwin";
-        extraSpecialArgs = {inherit inputs;};
+      # config for macbook
+      darwinConfigurations.${macbook} = nix-darwin.lib.darwinSystem {
+        specialArgs = { inherit inputs; };
         modules = [
-            ./hosts/${macbook}/configuration.nix
+          ./hosts/${macbook}/configuration.nix
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              # Apple Silicon Only
+              enableRosetta = true;
+
+              user = "rohandatar";
+
+              autoMigrate = true;
+            };
+          }
+        ];
+      };
+
+      homeConfigurations = {
+        rdatar = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages."x86_64-linux";
+          extraSpecialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/${home-desktop}/home.nix
             catppuccin.homeManagerModules.catppuccin
-        ];
+          ];
+        };
+
+        rohandatar = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+          extraSpecialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/${macbook}/home.nix
+            catppuccin.homeManagerModules.catppuccin
+          ];
+        };
       };
     };
-  };
 }
