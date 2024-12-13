1464bcd01eff1c5166d062878d8253ee7f0b1541
Rohan Datar <me@rohandatar.com>
2024-12-12 21:21:57 -0600

    145 current  2024-12-12 21:21:51  25.05.20241209.a73246e   6.12.3                          *

--- home-manager/platforms/linux/hypr/hyprlock.nix
+++ home-manager/platforms/linux/hypr/hyprlock.nix
@@ -1,11 +1,6 @@
 {...}: {
   programs.hyprlock = {
     enable = true;
-    catppuccin = {
-      enable = false;
-      # flavor = "mocha";
-      # accent = "blue";
-    };
 
     settings = {
       # GENERAL
@@ -67,8 +62,8 @@
         inner_color = "$surface0";
         font_color = "$text";
         fade_on_empty = "false";
-        # placeholder_text = ''<span foreground="##$textAlpha"><i>󰌾 Logged in as </i><span foreground="##$accentAlpha">$USER</span></span>'';
-        placeholder_text = "󰌾 Logged in as $USER";
+        placeholder_text = ''<span foreground="##$textAlpha"><i>󰌾 Logged in as </i><span foreground="##$accentAlpha">$USER</span></span>'';
+        # placeholder_text = "󰌾 Logged in as $USER";
         hide_input = "false";
         check_color = "$accent";
         fail_color = "$red";
