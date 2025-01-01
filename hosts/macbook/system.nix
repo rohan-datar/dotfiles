83c39f758245aa64516ed1afd9f84fde5aa82cc1
Rohan Datar <me@rohandatar.com>
2024-12-31 23:08:11 -0600

    14   2024-12-31 23:08:06   (current)

--- hosts/macbook/system.nix
+++ hosts/macbook/system.nix
@@ -16,7 +16,7 @@
     app_target_base="$HOME/Applications"
     app_target="$app_target_base/$moniker"
     mkdir -p "$app_target"
-    ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
+    sudo ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
   '';
 
   system.defaults = {
