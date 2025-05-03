c0166cdd8f338a4703ab7d14de3e5556a3493749
Rohan Datar <me@rohandatar.com>
2025-05-03 15:58:25 -0500

    215 current  2025-05-03 14:59:41  25.05.20250503.7a2622e  6.12.26                          *

--- home-manager/platforms/linux/home.nix
+++ home-manager/platforms/linux/home.nix
@@ -45,38 +45,38 @@
 
   gtk = {
     enable = true;
-    # theme = {
-    #   name = "Catppuccin-GTK-Dark";
-    #   package = pkgs.magnetic-catppuccin-gtk;
-    # };
+    theme = {
+      name = "Catppuccin-GTK-Dark";
+      package = pkgs.magnetic-catppuccin-gtk;
+    };
 
-    # gtk3.extraConfig = {
-    #   Settings = ''
-    #     gtk-application-prefer-dark-theme=1
-    #   '';
-    # };
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
 
-    # gtk4.extraConfig = {
-    #   Settings = ''
-    #     gtk-application-prefer-dark-theme=1
-    #   '';
-    # };
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
   };
 
   home.sessionVariables.GTK_THEME = "Catppuccin-GTK";
 
-  # dconf.settings = {
-  #   "org/gnome/shell" = {
-  #     disable-user-extensions = false;
-  #     enabled-extensions = ["user-theme@gnome-shell-extensions.gcampax.github.com"];
-  #   };
-  #   "org/gnome/shell/extensions/user-theme" = {
-  #     name = "Catppuccin-GTK-Dark";
-  #   };
-  #   "org/gnome/desktop/interface" = {
-  #     color-scheme = "prefer-dark";
-  #   };
-  # };
+  dconf.settings = {
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = ["user-theme@gnome-shell-extensions.gcampax.github.com"];
+    };
+    "org/gnome/shell/extensions/user-theme" = {
+      name = "Catppuccin-GTK-Dark";
+    };
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
 
   programs.kitty = {
     enable = true;
