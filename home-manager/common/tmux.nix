1e6089a22e9dc460669bc285dc4f43d788b86b51
Rohan Datar <me@rohandatar.com>
2025-05-17 13:35:44 -0500

    225 current  2025-05-17 13:02:32  25.05.20250515.e06158e  6.14.6                           *

--- home-manager/common/tmux.nix
+++ home-manager/common/tmux.nix
@@ -36,37 +36,37 @@ in {
 
       tmuxPlugins.vim-tmux-navigator
 
-      {
-        plugin = tmuxPlugins.catppuccin;
-        extraConfig = ''
-          # overall colors
-          set -g @catppuccin_flavor 'mocha'
+      # {
+      #   plugin = tmuxPlugins.catppuccin;
+      #   extraConfig = ''
+      #     # overall colors
+      #     set -g @catppuccin_flavor 'mocha'
 
-          # show an icon for the current window instead of *
-          set -g @catppuccin_window_status "icon"
+      #     # show an icon for the current window instead of *
+      #     set -g @catppuccin_window_status "icon"
 
-          # set the right of the window to arrow style and have an arrow between the window number an text
-          set -g @catppuccin_window_right_separator "█"
-          set -g @catppuccin_window_middle_separator "  "
+      #     # set the right of the window to arrow style and have an arrow between the window number an text
+      #     set -g @catppuccin_window_right_separator "█"
+      #     set -g @catppuccin_window_middle_separator "  "
 
-          # color the currently active window
-          set -g @catppuccin_window_default_fill "none"
-          set -g @catppuccin_window_current_fill "all"
+      #     # color the currently active window
+      #     set -g @catppuccin_window_default_fill "none"
+      #     set -g @catppuccin_window_current_fill "all"
 
-          # show the currently running process in the window
-          set -g @catppuccin_window_default_text "#W"
-          set -g @catppuccin_window_current_text "#W"
+      #     # show the currently running process in the window
+      #     set -g @catppuccin_window_default_text "#W"
+      #     set -g @catppuccin_window_current_text "#W"
 
-          # set the style of the status module
-          set -g @catppuccin_status_left_separator "█"
-          set -g @catppuccin_status_middle_separator ""
-          set -g @catppuccin_status_connect_separator "yes"
-          set -g @catppuccin_status_fill "all"
+      #     # set the style of the status module
+      #     set -g @catppuccin_status_left_separator "█"
+      #     set -g @catppuccin_status_middle_separator ""
+      #     set -g @catppuccin_status_connect_separator "yes"
+      #     set -g @catppuccin_status_fill "all"
 
-          # set which modules to display
-          set -g @catppuccin_status_modules_right "date_time user session"
-        '';
-      }
+      #     # set which modules to display
+      #     set -g @catppuccin_status_modules_right "date_time user session"
+      #   '';
+      # }
 
       {
         plugin = tmuxPlugins.resurrect;
