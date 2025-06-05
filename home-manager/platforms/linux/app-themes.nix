cc8221d27a45718d5189d1f3af54a45bf643d6fd
Rohan Datar <me@rohandatar.com>
2025-06-04 22:02:47 -0500

    241 current  2025-06-04 22:00:35  25.11.20250603.c2a0396  6.15.0                          *

--- home-manager/platforms/linux/app-themes.nix
+++ home-manager/platforms/linux/app-themes.nix
@@ -7,41 +7,41 @@
 
   catppuccin.kvantum.accent = "blue";
 
-  catppuccin.gtk.enable = true;
-  catppuccin.gtk.accent = "blue";
+  # catppuccin.gtk.enable = true;
+  # catppuccin.gtk.accent = "blue";
 
-  # gtk = {
-  #   enable = true;
-  #   theme = {
-  #     name = "Catppuccin-GTK-Dark";
-  #     package = pkgs.magnetic-catppuccin-gtk;
-  #   };
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Catppuccin-GTK-Dark";
+      package = pkgs.magnetic-catppuccin-gtk;
+    };
 
-  #   gtk3.extraConfig = {
-  #     Settings = ''
-  #       gtk-application-prefer-dark-theme=1
-  #     '';
-  #   };
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
 
-  #   gtk4.extraConfig = {
-  #     Settings = ''
-  #       gtk-application-prefer-dark-theme=1
-  #     '';
-  #   };
-  # };
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
 
-  # home.sessionVariables.GTK_THEME = "Catppuccin-GTK";
+  home.sessionVariables.GTK_THEME = "Catppuccin-GTK";
 
-  # dconf.settings = {
-  #   "org/gnome/shell" = {
-  #     disable-user-extensions = false;
-  #     enabled-extensions = ["user-theme@gnome-shell-extensions.gcampax.github.com"];
-  #   };
-  #   "org/gnome/shell/extensions/user-theme" = {
-  #     name = "Catppuccin-GTK-Dark";
-  #   };
-  #   "org/gnome/desktop/interface" = {
-  #     color-scheme = "prefer-dark";
-  #   };
-  # };
+  dconf.settings = {
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = ["user-theme@gnome-shell-extensions.gcampax.github.com"];
+    };
+    "org/gnome/shell/extensions/user-theme" = {
+      name = "Catppuccin-GTK-Dark";
+    };
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
 }
