ef57e3ce35118f1442c86e6936615dc519e75b45
Rohan Datar <me@rohandatar.com>
2025-06-04 22:30:32 -0500

    242 current  2025-06-04 22:10:52  25.11.20250603.c2a0396  6.15.0                          *

--- home-manager/common/starship.nix
+++ home-manager/common/starship.nix
@@ -3,55 +3,61 @@
   lib,
   ...
 }: {
-  programs.starship = {
+  programs.starship = let
+    getPreset = name: (with builtins; removeAttrs (fromTOML (readFile "${pkgs.starship}/share/starship/presets/${name}.toml")) ["\"$schema\""]);
+  in {
     enable = true;
     enableZshIntegration = true;
     enableTransience = true;
 
     presets = ["nerd-font-symbols"];
 
-    settings = {
-      add_newline = true;
-      scan_timeout = 10;
+    settings =
+      lib.recursiveUpdate
+      (lib.mergeAttrsList [
+        (getPreset "nerd-font-symbols")
+      ]) {
+        add_newline = true;
+        scan_timeout = 10;
 
-      format = lib.concatStrings [
-        "╭─ "
-        "$os"
-        "$all"
-        "$line_break"
-        "╰─"
-        "$character"
-      ];
+        format = lib.concatStrings [
+          "╭─ "
+          "$os"
+          "$all"
+          "$line_break"
+          "╰─"
+          "$character"
+        ];
 
-      right_format = lib.concatStrings [
-        "$status"
-        " "
-        "$cmd_duration"
-      ];
+        right_format = lib.concatStrings [
+          "$status"
+          " "
+          "$cmd_duration"
+        ];
 
-      cmd_duration = {
-        format = "[ $duration]($style)";
+        cmd_duration = {
+          format = "[ $duration]($style)";
+        };
+
+        status = {
+          disabled = false;
+          format = "[$symbol $status]($style)";
+          symbol = "";
+          not_found_symbol = "";
+          not_executable_symbol = "";
+          sigint_symbol = "";
+          signal_symbol = "";
+        };
+
+        os = {
+          disabled = false;
+        };
+
+        directory = {
+          format = "[ $path ]($style)[$read_only ]($read_only_style)";
+          style = "bold blue";
+          read_only = " 󰌾";
+        };
       };
-
-      status = {
-        disabled = false;
-        format = "[$symbol $status]($style)";
-        symbol = "";
-        not_found_symbol = "";
-        not_executable_symbol = "";
-        sigint_symbol = "";
-        signal_symbol = "";
-      };
-
-      os = {
-        disabled = false;
-      };
-
-      directory = {
-        format = "[ $path ]($style)[$read_only ]($read_only_style)";
-        style = "bold blue";
-        read_only = " 󰌾";
-      };
-    };
   };
 }
