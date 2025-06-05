409b43b849468a738cb4b597cbea9432b9786584
Rohan Datar <me@rohandatar.com>
2025-06-04 22:26:25 -0500

    242 current  2025-06-04 22:10:52  25.11.20250603.c2a0396  6.15.0                          *

--- home-manager/common/starship.nix
+++ home-manager/common/starship.nix
@@ -8,6 +8,8 @@
     enableZshIntegration = true;
     enableTransience = true;
 
+    presets = ["nerd-font-symbols"];
+
     settings = {
       add_newline = true;
       scan_timeout = 10;
@@ -44,179 +46,11 @@
       os = {
         disabled = false;
       };
-      os.symbols = {
-        Alpaquita = " ";
-        Alpine = " ";
-        AlmaLinux = " ";
-        Amazon = " ";
-        Android = " ";
-        Arch = " ";
-        Artix = " ";
-        CentOS = " ";
-        Debian = " ";
-        DragonFly = " ";
-        Emscripten = " ";
-        EndeavourOS = " ";
-        Fedora = " ";
-        FreeBSD = " ";
-        Garuda = "󰛓 ";
-        Gentoo = " ";
-        HardenedBSD = "󰞌 ";
-        Illumos = "󰈸 ";
-        Kali = " ";
-        Linux = " ";
-        Mabox = " ";
-        Macos = " ";
-        Manjaro = " ";
-        Mariner = " ";
-        MidnightBSD = " ";
-        Mint = " ";
-        NetBSD = " ";
-        NixOS = " ";
-        OpenBSD = "󰈺 ";
-        openSUSE = " ";
-        OracleLinux = "󰌷 ";
-        Pop = " ";
-        Raspbian = " ";
-        Redhat = " ";
-        RedHatEnterprise = " ";
-        RockyLinux = " ";
-        Redox = "󰀘 ";
-        Solus = "󰠳 ";
-        SUSE = " ";
-        Ubuntu = " ";
-        Unknown = " ";
-        Void = " ";
-        Windows = "󰍲 ";
-      };
-
-      aws = {
-        symbol = "  ";
-      };
-
-      buf = {
-        symbol = " ";
-      };
-
-      c = {
-        symbol = " ";
-      };
-
-      conda = {
-        symbol = " ";
-      };
-
-      crystal = {
-        symbol = " ";
-      };
-
-      dart = {
-        symbol = " ";
-      };
 
       directory = {
         format = "[ $path ]($style)[$read_only ]($read_only_style)";
         style = "bold blue";
         read_only = " 󰌾";
-      };
-
-      docker_context = {
-        symbol = " ";
-      };
-
-      elixir = {
-        symbol = " ";
-      };
-
-      elm = {
-        symbol = " ";
-      };
-
-      fennel = {
-        symbol = " ";
-      };
-
-      fossil_branch = {
-        symbol = " ";
-      };
-
-      git_branch = {
-        symbol = " ";
-      };
-
-      git_status = {
-        format = lib.concatStrings [
-          "[$all_status$ahead_behind]($style)"
-        ];
-        style = "bold green";
-      };
-
-      git_commit = {
-        tag_symbol = "  ";
-      };
-
-      golang = {
-        symbol = " ";
-      };
-
-      guix_shell = {
-        symbol = " ";
-      };
-
-      haskell = {
-        symbol = " ";
-      };
-
-      haxe = {
-        symbol = " ";
-      };
-
-      hg_branch = {
-        symbol = " ";
-      };
-
-      hostname = {
-        ssh_symbol = " ";
-      };
-
-      java = {
-        symbol = " ";
-      };
-
-      julia = {
-        symbol = " ";
-      };
-
-      kotlin = {
-        symbol = " ";
-      };
-
-      lua = {
-        symbol = " ";
-      };
-
-      memory_usage = {
-        symbol = "󰍛 ";
-      };
-
-      meson = {
-        symbol = "󰔷 ";
-      };
-
-      nim = {
-        symbol = "󰆥 ";
-      };
-
-      nix_shell = {
-        symbol = " ";
-      };
-
-      nodejs = {
-        symbol = " ";
-      };
-
-      ocaml = {
-        symbol = " ";
       };
     };
   };
