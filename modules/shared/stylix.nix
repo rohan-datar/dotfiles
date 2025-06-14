7709460ce51004fc4bc6da8507e2b5ff8bc36205
Rohan Datar <me@rohandatar.com>
2025-06-14 17:24:43 -0500

    247 2025-06-14 17:24:28 6.15.2, NixOs

--- modules/shared/stylix.nix
+++ modules/shared/stylix.nix
@@ -7,26 +7,26 @@
     enable = true;
     base16Scheme = "${inputs.tinted-schemes}/base24/catppuccin-mocha.yaml";
     polarity = "dark";
-    fonts = {
-      serif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Serif";
-      };
+    # fonts = {
+    #   serif = {
+    #     package = pkgs.dejavu_fonts;
+    #     name = "DejaVu Serif";
+    #   };
 
-      sansSerif = {
-        package = pkgs.jetbrains-mono;
-        name = "JetBrains Mono Nerd Font Propo";
-      };
+    #   sansSerif = {
+    #     package = pkgs.jetbrains-mono;
+    #     name = "JetBrains Mono Nerd Font Propo";
+    #   };
 
-      monospace = {
-        package = pkgs.maple-mono.NF;
-        name = "Maple Mono NF";
-      };
+    #   monospace = {
+    #     package = pkgs.maple-mono.NF;
+    #     name = "Maple Mono NF";
+    #   };
 
-      emoji = {
-        package = pkgs.noto-fonts-emoji;
-        name = "Noto Color Emoji";
-      };
-    };
+    #   emoji = {
+    #     package = pkgs.noto-fonts-emoji;
+    #     name = "Noto Color Emoji";
+    #   };
+    # };
   };
 }
