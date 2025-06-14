f2d63ebcedf26170c72613763dabd24869a46fa0
Rohan Datar <me@rohandatar.com>
2025-06-14 16:53:07 -0500

    245 2025-06-14 16:36:02 6.15.2, NixOs

---
+++ modules/shared/stylix.nix
@@ -0,0 +1,32 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  stylix = {
+    enable = true;
+    base16Scheme = "${inputs.tinted-schemes}/schemes/base24/catppuccin-mocha.yaml";
+    polarity = "dark";
+    fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.jetbrains-mono;
+        name = "JetBrains Mono Nerd Font Propo";
+      };
+
+      monospace = {
+        package = pkgs.maple-mono.NF;
+        name = "Maple Mono NF";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
+  };
+}
