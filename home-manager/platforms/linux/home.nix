3a9f29c7a44747c9f9e73ff1850ecad043aecae7
Rohan Datar <me@rohandatar.com>
2025-06-15 21:08:11 -0500

    256 2025-06-15 19:11:27 6.15.2, NixOs

--- home-manager/platforms/linux/home.nix
+++ home-manager/platforms/linux/home.nix
@@ -22,7 +22,20 @@
     networkmanager_dmenu
     networkmanagerapplet
     ghostty
+
+    #fonts
+
+    maple-mono.NF
+    nerd-fonts.meslo-lg
+    font-awesome
+    inputs.apple-fonts.packages.${pkgs.system}.sf-pro
+    inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd
+    inputs.apple-fonts.packages.${pkgs.system}.sf-compact
+    inputs.apple-fonts.packages.${pkgs.system}.sf-compact-nerd
+    inputs.apple-fonts.packages.${pkgs.system}.ny
+    inputs.apple-fonts.packages.${pkgs.system}.ny-nerd
   ];
+  fonts.fontconfig.enable = true;
 
   programs.fzf.enable = true;
 
