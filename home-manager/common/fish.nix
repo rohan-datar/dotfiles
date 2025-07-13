f49710b9b31caa4413f9d40bf2cefd07c6400bf1
Rohan Datar <me@rohandatar.com>
2025-07-13 02:24:48 -0500

    46 25/07/13 02:24:34 , MacOs

--- home-manager/common/fish.nix
+++ home-manager/common/fish.nix
@@ -54,6 +54,7 @@ in {
       fish_user_key_bindings = {
         body = ''
           # Execute this once per mode that emacs bindings should be used in
+          fish_default_key_bindings -M insert
 
           # Then execute the vi-bindings so they take precedence when there's a conflict.
           # Without --no-erase fish_vi_key_bindings will default to
