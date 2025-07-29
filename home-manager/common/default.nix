31b262b7b768f95e4c8c8e2f2b55a9f6df61d4fd
Rohan Datar <me@rohandatar.com>
2025-07-28 23:18:51 -0500

    53 25/07/28 23:18:39: MacOs

--- home-manager/common/default.nix
+++ home-manager/common/default.nix
@@ -85,6 +85,26 @@
         size = pkgs.lib.mkForce 14;
       };
     };
+    direnv = {
+      silent = true;
+
+      # faster, persistent implementation of use_nix and use_flake
+      nix-direnv.enable = true;
+
+      # store direnv in cache and not per project
+      # <https://github.com/direnv/direnv/wiki/Customizing-cache-location#hashed-directories>
+      stdlib = ''
+        : ''${XDG_CACHE_HOME:=$HOME/.cache}
+        declare -A direnv_layout_dirs
+
+        direnv_layout_dir() {
+          echo "''${direnv_layout_dirs[$PWD]:=$(
+            echo -n "$XDG_CACHE_HOME"/direnv/layouts/
+            echo -n "$PWD" | sha1sum | cut -d ' ' -f 1
+          )}"
+        }
+      '';
+    };
   };
 
   # Home Manager can also manage your environment variables through
