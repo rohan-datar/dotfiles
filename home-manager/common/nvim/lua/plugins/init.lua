595d72433dd753c773e6cf55d31f7aeb23c095d1
Rohan Datar <me@rohandatar.com>
2025-08-19 22:47:57 -0500

    64 25/08/19 22:47:40: MacOs

--- home-manager/common/nvim/lua/plugins/init.lua
+++ home-manager/common/nvim/lua/plugins/init.lua
@@ -2,8 +2,6 @@ return {
 	{ "tpope/vim-sleuth" }, -- Detect tabstop and shiftwidth automatically
 	{ "tpope/vim-surround", event = "VeryLazy" },
 	{ "tpope/vim-repeat", event = "VeryLazy" },
-	{ "tpope/vim-dadbod", event = "VeryLazy" },
-	{ "github/copilot.vim" },
 	{
 		"nvimdev/hlsearch.nvim",
 		event = "BufRead",
