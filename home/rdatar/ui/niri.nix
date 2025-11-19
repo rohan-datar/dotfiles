875ff48236bac7dd7f66fac79cc5a6f932f3d76b
Rohan Datar <me@rdatar.com>
2025-11-19 04:26:44 +0000

    nx switch (home-media/NixOS): sys=58 @ 2025-11-19T04:26:44Z

--- home/rdatar/ui/niri.nix
+++ home/rdatar/ui/niri.nix
@@ -1,70 +1,80 @@
 {
-  programs.niri = {
-    settings = {
-      binds = {
-        "Mod+Space".action.spawn = [
-          "qs"
-          "-c"
-          "noctalia-shell"
-          "ipc"
-          "call"
-          "launcher"
-          "toggle"
-        ];
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+in
+{
+  config = mkIf config.olympus.aspects.graphical.enable {
+    programs.niri = {
+      settings = {
+        binds = {
+          "Mod+Space".action.spawn = [
+            "qs"
+            "-c"
+            "noctalia-shell"
+            "ipc"
+            "call"
+            "launcher"
+            "toggle"
+          ];
 
-        "Mod+Return".action.spawn = "ghostty";
-        "Mod+B".action.spawn = "zen";
+          "Mod+Return".action.spawn = "ghostty";
+          "Mod+B".action.spawn = "zen";
 
-        "XF86AudioRaiseVolume".action.spawn = [
-          "qs"
-          "-c"
-          "noctalia-shell"
-          "ipc"
-          "call"
-          "volume"
-          "increase"
-        ];
-        "XF86AudioLowerVolume".action.spawn = [
-          "qs"
-          "-c"
-          "noctalia-shell"
-          "ipc"
-          "call"
-          "volume"
-          "decrease"
-        ];
-        "XF86AudioMute".action.spawn = [
-          "qs"
-          "-c"
-          "noctalia-shell"
-          "ipc"
-          "call"
-          "volume"
-          "muteOutput"
-        ];
+          "XF86AudioRaiseVolume".action.spawn = [
+            "qs"
+            "-c"
+            "noctalia-shell"
+            "ipc"
+            "call"
+            "volume"
+            "increase"
+          ];
+          "XF86AudioLowerVolume".action.spawn = [
+            "qs"
+            "-c"
+            "noctalia-shell"
+            "ipc"
+            "call"
+            "volume"
+            "decrease"
+          ];
+          "XF86AudioMute".action.spawn = [
+            "qs"
+            "-c"
+            "noctalia-shell"
+            "ipc"
+            "call"
+            "volume"
+            "muteOutput"
+          ];
 
-        "XF86MonBrightnessUp".action.spawn = [
-          "qs"
-          "-c"
-          "noctalia-shell"
-          "ipc"
-          "call"
-          "brightness"
-          "increase"
-        ];
+          "XF86MonBrightnessUp".action.spawn = [
+            "qs"
+            "-c"
+            "noctalia-shell"
+            "ipc"
+            "call"
+            "brightness"
+            "increase"
+          ];
 
-        "XF86MonBrightnessDown".action.spawn = [
-          "qs"
-          "-c"
-          "noctalia-shell"
-          "ipc"
-          "call"
-          "brightness"
-          "decrease"
-        ];
+          "XF86MonBrightnessDown".action.spawn = [
+            "qs"
+            "-c"
+            "noctalia-shell"
+            "ipc"
+            "call"
+            "brightness"
+            "decrease"
+          ];
+        };
+
+        input.focus-follows-mouse.enable = true;
       };
-
-      input.focus-follows-mouse.enable = true;
     };
   };
 }
