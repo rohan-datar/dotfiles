1c21224f1bc5f47ce523d6ffb6a52b9157adb366
Rohan Datar <me@rohandatar.com>
2026-07-12 00:47:41 -0500

    nx switch (home-desktop/NixOS): sys=537 @ 2026-07-12T05:47:41Z

--- modules/wrapped/direnv.nix
+++ modules/wrapped/direnv.nix
@@ -35,11 +35,7 @@ _: {
           declare -A direnv_layout_dirs
 
           direnv_layout_dir() {
-            echo "''${direnv_layout_dirs[$PWD]:=$
-              (
-                echo -n "$XDG_CACHE_HOME"/direnv/layouts/
-                echo -n "$PWD" | sha1sum | cut -d ' ' -f 1
-              )}"
+            echo "''${direnv_layout_dirs[$PWD]:=$XDG_CACHE_HOME/direnv/layouts/$(echo -n "$PWD" | sha1sum | cut -d ' ' -f 1)}"
           }
         '';
       };
