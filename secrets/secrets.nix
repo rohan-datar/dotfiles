2c5613b0d6329c7cc8e990b4b7cb22e29c1be5bd
Rohan Datar <me@rohandatar.com>
2026-07-25 18:16:12 -0500

    Add secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -62,5 +62,8 @@ in
   "nix-access-tokens.conf.age".publicKeys = allKeys;
   "smtp-password.age".publicKeys = home-nas-keys ++ rdatar-macbook;
   "keycloak-db-password.age".publicKeys = home-controller-keys;
-  "oauth2-proxy-env.age".publicKeys = home-controller-keys;
+  "lldap-env.age".publicKeys = home-controller-keys ++ rdatar-macbook;
+  "lldap-admin-password.age".publicKeys = home-controller-keys ++ rdatar-macbook;
+  "oauth2-proxy-env.age".publicKeys = home-media-keys;
+  "cloudflare-dns-token.age".publicKeys = home-media-keys;
 }
