23d84acfa2ecbcd4f89d6ec2f1f1c045dde60573
Rohan Datar <me@rohandatar.com>
2026-07-25 14:18:31 -0500

    Set up keycloak

---
+++ modules/nixos/controller/keycloak.nix
@@ -0,0 +1,26 @@
+_: {
+  flake.modules.nixos.keycloak =
+    { config, ... }:
+    {
+      age.secrets.keycloak-db-password.file = ../../../secrets/keycloak-db-password.age;
+
+      services.keycloak = {
+        enable = true;
+        database = {
+          type = "postgresql";
+          createLocally = true;
+          passwordFile = config.age.secrets.keycloak-db-password.path;
+        };
+
+        settings = {
+          hostname = "auth.datars.org";
+          http-enabled = true;
+          http-host = "0.0.0.0";
+          http-port = 8080;
+          proxy-headers = "xforwarded";
+        };
+      };
+
+      networking.firewall.interfaces.br0.allowedTCPPorts = [ 8080 ];
+    };
+}
