e3d36329f0a280322fd91cff85c359afe504ef14
Rohan Datar <me@rohandatar.com>
2026-09-05 00:57:26 -0500

    nx switch (home-desktop/NixOS): sys=552 @ 2026-09-05T05:57:26Z

--- modules/nixos/logs.nix
+++ modules/nixos/logs.nix
@@ -2,10 +2,10 @@
   flake.modules.nixos.logs = {
     # limit systemd journal size
     # https://wiki.archlinux.org/title/Systemd/Journal#Persistent_journals
-    services.journald.settings.Journal = ''
-      SystemMaxUse=100M
-      RuntimeMaxUse=50M
-      SystemMaxFileSize=50M
-    '';
+    services.journald.settings.Journal = {
+      SystemMaxFileSize = "50M";
+      SystemMaxUse = "100M";
+      RuntimeMaxUse = "50M";
+    };
   };
 }
