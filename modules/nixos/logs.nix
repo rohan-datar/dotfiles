46ba348e5c044166273a54338156c877309ca4e2
Rohan Datar <me@rohandatar.com>
2026-09-05 00:32:51 -0500

    nx switch (Rohans-MacBook/macOS): sys=261 @ 2026-09-05T05:32:51Z

--- modules/nixos/logs.nix
+++ modules/nixos/logs.nix
@@ -2,7 +2,7 @@
   flake.modules.nixos.logs = {
     # limit systemd journal size
     # https://wiki.archlinux.org/title/Systemd/Journal#Persistent_journals
-    services.journald.extraConfig = ''
+    services.journald.settings.Journal = ''
       SystemMaxUse=100M
       RuntimeMaxUse=50M
       SystemMaxFileSize=50M
